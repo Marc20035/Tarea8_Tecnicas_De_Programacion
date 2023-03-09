e2591dd25edb7bd9a3914a173ab6bd99bb635834
tHeCreator <marcsabalete@gmail.com>
2023-03-09 21:26:35 +0000

    Ejercicio2(Version2)

--- Ejercicio2.java
+++ Ejercicio2.java
@@ -92,4 +92,102 @@ public class Pedido {
     }
 }
 
-
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Cliente> clientes = new ArrayList<Cliente>();
+        ArrayList<Pedido> pedidos = new ArrayList<Pedido>();
+        int opcion = 0;
+        do {
+            System.out.println("1 - Mostrar clientes");
+            System.out.println("2 - Crear clientes");
+            System.out.println("3 - Crear una obra");
+            System.out.println("4 - Crear un pedido");
+            System.out.println("5 - Mostrar pedidos");
+            System.out.println("6 - Salir");
+            opcion = sc.nextInt();
+            switch (opcion) {
+                case 1:
+                    for (Cliente cliente : clientes) {
+                        System.out.println(cliente);
+                    }
+                    break;
+                case 2:
+                    System.out.println("Ingrese el nombre del cliente");
+                    String nombre = sc.next();
+                    System.out.println("Ingrese el apellido del cliente");
+                    String apellido = sc.next();
+                    System.out.println("Ingrese el dni del cliente");
+                    int dni = sc.nextInt();
+                    System.out.println("Ingrese el telefono del cliente");
+                    int telefono = sc.nextInt();
+                    System.out.println("Ingrese el email del cliente");
+                    String email = sc.next();
+                    Cliente cliente = new Cliente(nombre, apellido, dni, telefono, email);
+                    clientes.add(cliente);
+                    break;
+                case 3:
+                    System.out.println("Ingrese el titulo de la obra");
+                    String titulo = sc.next();
+                    System.out.println("Ingrese el autor de la obra");
+                    String autor = sc.next();
+                    System.out.println("Ingrese el tipo de obra (1 - Libro, 2 - Video)");
+                    int tipoObra = sc.nextInt();
+                    if (tipoObra == 1) {
+                        System.out.println("Ingrese el numero de paginas");
+                        int numPaginas = sc.nextInt();
+                        Libro libro = new Libro(titulo, autor, numPaginas);
+                    } else {
+                        System.out.println("Ingrese la duracion");
+                        int duracion = sc.nextInt();
+                        Video video = new Video(titulo, autor, duracion);
+                    }
+                    break;
+                case 4:
+                    System.out.println("Ingrese el dni del cliente");
+                    int dniCliente = sc.nextInt();
+                    Cliente clientePedido = null;
+                    for (Cliente clienteAux : clientes) {
+                        if (clienteAux.getDni() == dniCliente) {
+                            clientePedido = clienteAux;
+                        }
+                    }
+                    if (clientePedido != null) {
+                        Pedido pedido = new Pedido(clientePedido);
+                        System.out.println("Ingrese el titulo de la obra");
+                        String tituloObra = sc.next();
+                        System.out.println("Ingrese el autor de la obra");
+                        String autorObra = sc.next();
+                        System.out.println("Ingrese el tipo de obra (1 - Libro, 2 - Video)");
+                        int tipoObraPedido = sc.nextInt();
+                        if (tipoObraPedido == 1) {
+                            System.out.println("Ingrese el numero de paginas");
+                            int numPaginas = sc.nextInt();
+                            Libro libro = new Libro(tituloObra, autorObra, numPaginas);
+                            pedido.agregarObra(libro);
+                        } else {
+                            System.out.println("Ingrese la duracion");
+                            int duracion = sc.nextInt();
+                            Video video = new Video(tituloObra, autorObra, duracion);
+                            pedido.agregarObra(video);
+                        }
+                        pedidos.add(pedido);
+                    } else {
+                        System.out.println("No se encontro el cliente");
+                    }
+                    break;
+                case 5:
+                    for (Pedido pedido : pedidos) {
+                        System.out.println(pedido);
+                    }
+                    break;
+                case 6:
+                    System.out.println("Hasta luego");
+                    break;
+                default:
+                    System.out.println("Opcion incorrecta");
+                    break;
+            }
+        } while (opcion != 6);
+    }
+}
